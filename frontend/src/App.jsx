6b3528f91acdc7801f78fef35eef667f3d9f3819
Sk8Mahammad-Irfan9 <lazybum729@gmail.com>
2025-07-18 12:58:56 +0530

    added vercel.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
-import PrivateRoutes from "./Routes/private"; // Your PrivateRoute component
+import PrivateRoutes from "./Routes/private";
 import HomePage from "./pages/HomePage";
 import NotApproved from "./pages/NotApproved";
 import UserProfileForm from "./pages/userDetails/UserProfileForm";
